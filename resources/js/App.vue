8f0840b899258ac7e1963904a5c3525f4b9267b4
SorndevT <sornde.teach@gmail.com>
2024-10-16 20:04:01 +0700

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        Hello App Vue hgdfjshgdfjhjfdkshjfdh4564454 4848487
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
