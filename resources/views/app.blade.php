7a057aef3ef2bdb034e44ff1fb7b994c173fb69a
SornDev <sorndev.teach@gmail.com>
2024-10-16 21:11:53 +0700

    install template HTML5 VDO 02

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App MiniPos 20</title>
+
+    
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css">
+    <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css">
+    <link rel="stylesheet" href="../assets/css/demo.css">
+    
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css"> 
+    <link rel="stylesheet" href="../assets/vendor/libs/apex-charts/apex-charts.css">
+
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    
+    <!-- Page CSS -->
+
     @vite('resources/css/app.css')
 </head>
 <body>
-        <h1> Hello Laravel App </h1> 
-        <hr>
+
         <div id="app-vue"></div>
+
+
+  <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+  <script src="../assets/vendor/libs/popper/popper.js"></script>
+  <script src="../assets/vendor/js/bootstrap.js"></script>
+  <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+  <script src="../assets/vendor/js/menu.js"></script>
+  
 
     @vite('resources/js/app.js')
 </body>
